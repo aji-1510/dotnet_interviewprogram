06e5302670ae20f22e2d39a74d8cd4f376a08123
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-01 01:25:08 -0700

    Add files via upload

---
+++ reversNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace reversNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            Console.WriteLine("enter the number of element");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            Console.WriteLine("\n Enter the {0} number", n);
+            for(i=0;i<n;i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("revers number");
+           
+            for(i=a.Length-1;i>=0;i--)
+            {
+                Console.WriteLine(a[i]);
+
+            }
+        }
+    }
+}
