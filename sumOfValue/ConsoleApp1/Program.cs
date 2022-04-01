990551752a9b53872e212abfd7b126c63a1fa5ac
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-01 00:55:05 -0700

    Add files via upload

---
+++ sumOfValue/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            do
+            {
+                Console.WriteLine("Enter the number");
+                int n = Int32.Parse(Console.ReadLine());
+                sum_method(n);
+                Console.WriteLine("\n\nDo you want run again Y/N");
+                ch =Convert.ToChar(Console.ReadLine());
+
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
+            static void sum_method(int n)
+            {
+                int sum = 0, m;
+                while (n > 0)
+                {
+                    m = n % 10;
+                    sum = sum + m;
+                    n = n / 10;
+                }
+                
+                Console.Write("Sum of value= " + sum);
+
+            }
+        
+    }
+}
