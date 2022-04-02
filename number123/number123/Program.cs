c624146313c5087647507f0505df01632e8be453
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-02 07:07:02 -0400

    Add files via upload

---
+++ number123/number123/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace number123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0,j=0;
+            for(i=0;i<=5;i++)
+            {
+                for(j=0;j<i;j++)
+                {
+                    Console.Write("i");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
