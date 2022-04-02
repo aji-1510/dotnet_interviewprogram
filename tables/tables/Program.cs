94314b0c2cad9718f4e0c9150b27194d4aa25a55
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-02 08:26:48 -0400

    Add files via upload

---
+++ tables/tables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            do
+            {
+                int n;
+                Console.WriteLine("which table you want see");
+                n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine(+i + "*" + n + "=" + (i * n));
+                }
+                Console.WriteLine("Do you want run again Y/N");
+                ch = Convert.ToChar(Console.ReadLine());
+
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
+    }
+}
