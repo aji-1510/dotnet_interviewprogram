8b4e6b712741494fea3948f8c6413f51ec34107c
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-02 07:08:51 -0400

    Add files via upload

---
+++ oddandEven/oddandEven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace oddandEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.WriteLine("enter the number ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            if((num1%2)==0)
+            {
+                Console.WriteLine("this is even number");
+
+            }
+            else
+            {
+                Console.WriteLine("this is odd number");
+            }
+
+        }
+    }
+}
