3211134ec6b16f4a9feb7f5498c6f337c21d10f6
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-02 07:54:08 -0400

    Add files via upload

---
+++ vowels/vowels/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace vowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char yes, ch;
+            do
+            {
+                Console.WriteLine("Enter the any single letter");
+                ch = Convert.ToChar(Console.ReadLine());
+                if ((ch == 'a') || (ch == 'A') || (ch == 'e') || (ch == 'E') || (ch == 'i') || (ch == 'I') || (ch == 'o') || (ch == 'O') || (ch == 'u') || (ch == 'U'))
+                {
+                    Console.WriteLine("{0} this word  is vowel", ch);
+                }
+                else
+                {
+                    Console.WriteLine("{0}  This word is not vowel", ch);
+
+                }
+                Console.WriteLine("Do you want run again Y/N");
+                yes = Convert.ToChar(Console.ReadLine());
+
+            } while ((yes == 'y') || (yes == 'Y'));
+
+
+        }
+    }
+}
