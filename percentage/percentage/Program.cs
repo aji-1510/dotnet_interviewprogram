3d32f7d4caade97bf7950f29d1e706faf0f9cbca
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-02 07:17:50 -0400

    Add files via upload

---
+++ percentage/percentage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace percentage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int subj1, subj2, subj3, subj4, subj5, total;
+            float per;
+            char ch;
+            lable:
+            Console.WriteLine("enter the subj1 mark");
+            subj1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the subj2 mark");
+            subj2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the subj3 mark");
+            subj3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the subj4 mark");
+            subj4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the subj5 mark");
+            subj5 = int.Parse(Console.ReadLine());
+            total = (subj1 + subj2 + subj3 + subj4 + subj5);
+            per = total / 5;
+            Console.WriteLine("your total mark" + total);
+            Console.WriteLine("\n\nyour total percentage " +per+"%");
+            Console.WriteLine("do you want run again Y/N");
+            ch = Convert.ToChar(Console.ReadLine());
+            if ((ch == 'Y') || (ch == 'y'))
+             {
+                goto lable;
+            }
+
+        }
+    }
+}
