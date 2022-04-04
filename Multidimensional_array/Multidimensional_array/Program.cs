5952085cff2a8d91ee9b090a44180b15c37004b0
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-04 10:46:11 +0530

    Add files via upload

---
+++ Multidimensional_array/Multidimensional_array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Multidimensional_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 =new int[3,3];
+            int[,] arr2 = new int[3, 3];
+            int i, j;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("enter the array value:\t");
+                    arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+
+            for(i=0;i<3;i++)
+             {
+                for(j=0;j<3;j++)
+                 {
+                    arr2[i, j] = arr1[i, j];
+
+                 }
+             }
+            Console.WriteLine("\n\n elements of second array are:\n\n");
+            for(i=0;i<3;i++)
+            {
+                Console.WriteLine();
+                for(j=0;j<3;j++)
+                {
+                    Console.Write("\t{0}", arr2[i, j]);
+
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
