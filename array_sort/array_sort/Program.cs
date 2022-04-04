d8d228ef708071743a316d4c9781431f52e7a5f3
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-03 23:25:54 -0400

    Add files via upload

---
+++ array_sort/array_sort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace array_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("\n");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] =int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("--------------your array elements--------------");
+            Console.WriteLine("\n");
+            foreach (int j in arr)
+            {
+                Console.WriteLine(j);
+            }
+            Array.Sort(arr);
+            Console.WriteLine("--------------sort array---------------");
+            Console.WriteLine("\n");
+            foreach (int j in arr)
+            {
+                Console.WriteLine(j);
+            }
+
+        }
+    }
+}
