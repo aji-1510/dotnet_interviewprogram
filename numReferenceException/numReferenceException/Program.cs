552827ba32455cacbe11dc4789daa55f7b4069b4
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-04 11:42:14 +0530

    Add files via upload

---
+++ numReferenceException/numReferenceException/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace numReferenceException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = null;
+            try
+            {
+                int length = value.Length;
+                Console.WriteLine(length);
+
+                Console.ReadLine();
+            }
+            catch (NullReferenceException ne)
+            {
+                Console.WriteLine(ne.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
