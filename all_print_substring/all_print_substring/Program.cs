b7eb3b7873149d2bb54cbf6a4c79da03a39af712
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-09 13:42:05 +0530

    Add files via upload

---
+++ all_print_substring/all_print_substring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace all_print_substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words;
+            Console.WriteLine("---------------------Enter the your name-------------------");
+            words = Console.ReadLine();
+            for (int i = 1; i < words.Length; i++)
+            {
+                for (int start = 0; start <= words.Length - i; start++)
+                {
+                    string substr = words.Substring(start, i);
+                    Console.WriteLine(substr);
+                }
+            }
+        }
+    }
+}
