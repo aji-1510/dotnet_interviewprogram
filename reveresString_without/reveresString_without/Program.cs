146f2142a53c16263ab4248971b35b49cdad967f
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-09 13:57:32 +0530

    Add files via upload

---
+++ reveresString_without/reveresString_without/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace reveresString_without
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("----Enter the your name----");
+            name = Console.ReadLine();
+            char[] revers = name.ToCharArray();
+            string name2 = "";
+            for (int i=revers.Length-1;i>=0;i--)
+            {
+                name2 += revers[i];
+            }
+            Console.WriteLine(name2);
+            Console.ReadKey();
+        }
+    }
+}
