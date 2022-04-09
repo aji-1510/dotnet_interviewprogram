21ae814b4ec03481c8165d2cb3a8a0c8e264f5c4
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-09 13:27:15 +0530

    Add files via upload

---
+++ substring/substring/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = "C# Program to List all Substrings in a given String";
+            string find = words.Substring(23, 10);
+            Console.WriteLine(words);
+            Console.WriteLine("\n{0}", find);
+            Console.ReadKey();
+        }
+    }
+}
