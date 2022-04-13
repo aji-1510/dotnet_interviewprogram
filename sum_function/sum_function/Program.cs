6b4e35003a97c96de53066bbaed7ed3f7b6768cc
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-13 10:49:39 +0530

    Add files via upload

---
+++ sum_function/sum_function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sum_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("\n\nFunction to calculate the sum of two numbers :\n");
+            Console.Write("Enter a number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n",sum(n1, n2));
+        }
+        static int sum(int num1,int num2)
+        {
+            int total;
+            total = num1 + num2;
+            return total;
+        }
+
+    }
+}
