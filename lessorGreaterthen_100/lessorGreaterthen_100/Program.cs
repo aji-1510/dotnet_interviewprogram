c8ede363e5b098b9b3e86f208e292dff026bfc02
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-18 12:32:37 +0530

    Add files via upload

---
+++ lessorGreaterthen_100/lessorGreaterthen_100/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lessorGreaterthen_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n;
+            Console.Write("Enter the first number (<100): ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number (>200): ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((m < 100 && n > 200));
+
+            Console.ReadKey();
+        }
+    }
+}
