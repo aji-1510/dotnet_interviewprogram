6879d68ac5c253149fab6c91632e5d3cc70efe80
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-18 12:49:33 +0530

    Add files via upload

---
+++ sumOFfirstTennumber/sumOFfirstTennumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_natural_nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a C# Sharp program to find the sum of first 10 natural numbers */
+            int i, sum = 0;
+            Console.WriteLine("The first 10 natural numbers");
+            for (i = 1; i <= 10; i++)
+            {
+                sum = sum + i;
+                Console.Write("{0} ", i);
+            }
+            Console.Write("\nThe Sum is : {0}", sum);
+
+            Console.ReadKey();
+        }
+    }
+}
